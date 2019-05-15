9eadbe9cf78357f1356940eb30473dc2a4c95d6f
Artur Tamborski <tamborskiartur@gmail.com>
2019-05-15 22:41:21 +0200

    remove newline

---
+++ roles/apt/files/keys.sh
@@ -0,0 +1,16 @@
+apt-key adv --fetch-keys https://www.virtualbox.org/download/oracle_vbox_2016.asc
+apt-key adv --fetch-keys https://www.virtualbox.org/download/oracle_vbox.asc
+apt-key adv --fetch-keys https://download.docker.com/linux/debian/gpg
+apt-key adv --fetch-keys https://updates.signal.org/desktop/apt/keys.asc
+apt-key adv --fetch-keys https://dl.sinew.in/keys/enpass-linux.key
+apt-key adv --keyserver  ha.pool.sks-keyservers.net --recv-keys EAC0D406E5D79A82ADEEDFDFB76E53652D87398A
+apt-key adv --keyserver  ha.pool.sks-keyservers.net --recv-keys EF3F38C8FD5E1EE2E7B3B657653124679B3CCB19
+apt-key adv --keyserver  ha.pool.sks-keyservers.net --recv-keys 3766223989993A70
+apt-key adv --keyserver  ha.pool.sks-keyservers.net --recv-keys E58A9D36647CAE7F
+apt-key adv --keyserver  ha.pool.sks-keyservers.net --recv-keys A87FF9DF48BF1C90
+apt-key adv --keyserver  ha.pool.sks-keyservers.net --recv-keys 818A435C5FCBF54A
+apt-key adv --keyserver  ha.pool.sks-keyservers.net --recv-keys 2CC26F777B8B44A1
+apt-key adv --keyserver  keyserver.ubuntu.com       --recv-key AD319E0F7CFFA38B4D9F6E55CE3F3DE92099F7A4
+apt-key adv --keyserver  keyserver.ubuntu.com       --recv-keys C80E383C3DE9F082E01391A0366C67DE91CA5D5F
+apt-key adv --keyserver  keyserver.ubuntu.com       --recv-keys F24AEA9FB05498B7
+apt-key adv --keyserver  keyserver.ubuntu.com       --recv-keys 2667CA5C
